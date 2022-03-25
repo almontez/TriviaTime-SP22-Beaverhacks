645366f59e2e059387bf7039b2dd05170f127863
Abby Thornton <thorntab@oregonstate.edu>
2022-03-25 00:34:52 -0500

    Fixed typo in parameter

--- math_quiz_data.py
+++ math_quiz_data.py
@@ -2,7 +2,7 @@ import requests
 
 parameters = {
     "amount": 10,
-    "type": "muliple"
+    "type": "multiple"
 }
 
 response = requests.get(url="https://opentdb.com/api.php?amount=10&category=19&difficulty=easy&type=multiple",
